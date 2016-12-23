5a23966bef89744a492f2972f845ddf9ceb4cbc2
ykxie <ykxie@kaikeba.com>
2016-12-23 10:04:49 +0800

    add 1.1 tag

--- js/Timer-1.1.js
+++ js/Timer-1.1.js
@@ -1,34 +1,33 @@
 (function(window){
     var Timer = {
         options: {
-            timer: null,
-            dom: null,
-            seconds: 1,
-            status: 1
+            seconds: 1,     // 秒数
+            timer: null,    // 计时器
+            dom: null,      // 存入dom节点
+            status: 1       // 计时状态 1: 计时中  0: 暂停中
         },
-        init: function(){
-            console.log();
+
+        init: function(options){
+            var _this = this;
             this.options.dom = document.getElementById("timer");
             this.startTimer();
-            // if (dom) {
-            //    this.options.dom = dom;
-            //    this.startTimer(); 
-            // }else{
-            //     alert("没有传入DOM对象")
-            // }
+
+            this.options = $.extend(this.options, options);
+
         },
         getSeconds: function(){
             var seconds = Timer.options.seconds;
-            console.log(seconds);
+            return seconds
         },
-        getTimer: function(){
-            var _dom = this.options.dom;
+        formatTime: function(s){
+            var _dom = Timer.options.dom;
             var _status = this.options.status
-            var t = 1,
+            var seconds = this.getSeconds();
+            var time,
                 h = 0,
                 m = 0,
-                s
-            _status == 1 ? s = 1 : s = Timer.options.seconds;
+                s = seconds
+            // _status == 1 ? s = 1 : s = seconds;
                 
             return function(){
                 if (s > 0 && s % 60 == 0) {
@@ -39,38 +38,58 @@
                     h += 1;
                     m = 0;
                 }
-                t = (h < 10 ? '0' + h : h) + ":" + (m < 10 ? '0' + m : m) + ":" + (s < 10 ? '0' + s : s);
-                console.log(t)
-                _dom.innerHTML = t;
+                time = (h < 10 ? '0' + h : h) + ":" + (m < 10 ? '0' + m : m) + ":" + (s < 10 ? '0' + s : s);
+                console.log(_dom)
+
+                _dom.innerHTML = time;
                 _dom.value = Timer.options.seconds;
+
                 s += 1;
                 Timer.options.seconds += 1;
             }
         },
+
         startTimer: function(){
-            var _getTimer = this.getTimer();
+            console.log('++++++++++')
+            var _formatTime = this.formatTime();
             clearInterval(this.options.timer);
-            this.options.timer = setInterval(_getTimer, 1000);
+            this.options.timer = setInterval(_formatTime, 1000);
         },
-        getMilliSeconds: function(){
-            console.log(Timer.options.seconds);
+
+        pauseTimer: function(){
             var _status = this.options.status
             if (_status) {
-                this.stopTimer();
+                clearInterval(this.options.timer);
+                this.options.timer = null;
                 this.options.status = 0;
             }else{
                 this.startTimer();
                 this.options.status = 1;
             }
         },
+
         stopTimer: function(){
             clearInterval(this.options.timer);
             this.options.timer = null;
+            Timer.options.seconds = 1;
         }
     }
 
-    window.Timer = Timer;
+    window.Timer = window.Hourglass = Timer;
 
 })(window);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
